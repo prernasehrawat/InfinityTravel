93a7d819908c5c9de99c3643dabf02ef98f3b720
Pranav Srivatsa <pranav.srivatsa27@gmail.com>
2024-10-21 12:22:04 -0400

    Added sqlite connector and seedData script for initial database

--- server/README.md
+++ server/README.md
@@ -2,3 +2,13 @@
 
 - Run "bash setup.sh" to install necessary libraries and packages
 - Run "node server.js" to start the server
+
+# Db setup instructions
+
+- navigate to the server folder
+- run `npm i`.
+- run `sqlite3 infinity-travel.db` in the console. Leave the cli running.
+- Uncomment seedData() from the server.js file. This will create the tables and add initial data to it.
+- run your server with npm start.
+- Comment out seedData(), you won't need it after the first run.
+- Run select \* from users; in the sqlite cli. If you see 3 users, everything went well. You can exit the cli with .exit.
--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000; // Frontend will run on 3000, backend on 8000
-const cors = require('cors');
+const cors = require("cors");
+const { db, seedData } = require("./models/db");
 
 // const cors = require('cors');
-const loginRoute = require('./routes/login'); // Import the login route
+const loginRoute = require("./routes/login"); // Import the login route
 // Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
 });
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(cors());
 // Routes
-app.use('/login', loginRoute); // Use the login route
+app.use("/login", loginRoute); // Use the login route
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-});
 
+  // Uncomment this to seed data on the first run
+  // seedData();
+});
