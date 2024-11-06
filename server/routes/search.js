5ce8e1c840a76b16bd6ac9aaeb0fd81f78dbf082
Pranav Srivatsa <pranav.srivatsa27@gmail.com>
2024-11-06 15:47:09 -0500

    Bug fix on price range filters

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,50 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { db } = require("../models/db");
-//
-// // GET /search route
-// router.get("/", (req, res) => {
-//   const { stops, airline, maxPrice } = req.query;
-//
-//   let query = "SELECT * FROM flights WHERE 1=1";
-//   const params = [];
-//
-//   // Add filters based on query parameters
-//   if (stops) {
-//     query += " AND stops = ?";
-//     params.push(stops);
-//   }
-//   if (airline) {
-//     query += " AND airline = ?";
-//     params.push(airline);
-//   }
-//   if (maxPrice) {
-//     query += " AND base_cost <= ?";
-//     params.push(maxPrice);
-//   }
-//
-//   // Sort by base_cost in ascending order
-//   query += " ORDER BY base_cost ASC";
-//
-//   db.all(query, params, (err, rows) => {
-//     if (err) {
-//       console.error("Error retrieving flights:", err.message);
-//       return res.status(500).json({ error: "Internal server error" });
-//     }
-//
-//     if (rows.length === 0) {
-//       return res.status(404).json({
-//         message: "No flights found matching the search criteria.",
-//       });
-//     }
-//
-//     res.json(rows);
-//   });
-// });
-//
-// module.exports = router;
-//
-
 const express = require("express");
 const router = express.Router();
 const { db } = require("../models/db");
@@ -54,15 +7,15 @@ router.get("/", (req, res) => {
   const {
     stops,
     airlines,
-    priceRange,
+    price_range,
     origin_airport,
     arrival_airport,
     arrival_date,
   } = req.query;
 
   // Extract price range
-  const [minPrice, maxPrice] = priceRange
-    ? priceRange.split(",").map(Number)
+  const [minPrice, maxPrice] = price_range
+    ? price_range.split(",").map(Number)
     : [null, null];
 
   // Extract airlines
